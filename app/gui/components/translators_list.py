db827d815178f872486972fc37a91eca9f422b2c
hossein <hosseinhosseinzade1383@gmail.com>
2026-07-20 16:01:51 +0330

    scroll problem fixed on translators_list

--- app/gui/components/translators_list.py
+++ app/gui/components/translators_list.py
@@ -1,4 +1,6 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QListWidget, QLabel, QLineEdit
+#app/gui/components/translators_list
+
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QListWidget, QLabel, QLineEdit, QSizePolicy
 from PyQt6.QtCore import Qt
 
 from app.api.adapters.translators_data_adapter import TranslatorsDataAdapter
@@ -38,6 +40,7 @@ class TranslatorsListWidget(QWidget):
         # List Widget
         # =========================================================
         self.list_widget = QListWidget()
+        self.list_widget.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         layout.addWidget(self.list_widget)
 
         # =========================================================
